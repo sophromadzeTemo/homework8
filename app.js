88e592784c49287db0d0966a419ffdb3836d3244
sophromadzeTemo <sophromadzetemo@gmail.com>
2024-01-17 04:52:22 +0400

    done

---
+++ app.js
@@ -0,0 +1,26 @@
+// first tasks
+let name = "dan";
+
+alert(`hello ${name + 1}`); // hello dan1
+
+alert(`hello ${"name"}`); // hello name
+
+alert("hello ${name}"); // hello ${name}
+
+alert('hello ${"name"}'); // hello ${"name"}
+
+// second tasks
+"" + 1 + 0; // "10" (string)
+"" - 1 + 0; // -1 (number)
+true + false; // false megona mara turme 1 -ians wers
+6 / "3"; // 2 (number)
+"2" * "3"; // 6 (number)
+4 + 5 + px; // "45px" (string) megona mara "9px" (string) yofila
+"$" + 4 + 5; // "$45" (string)
+"4" - 2; // 2 (number)
+"4px" - 2; // NaN
+7 / 0; // infinity
+"-9" + 5; // "-95" (string)
+"-9" - 5; // -14 (number)
+null + 1; // 1
+undefined + 1; // undefined megona da NaN agmochnda.
